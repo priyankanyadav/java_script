491eb2d1364f8bc04aeb5ffb970f1a451052c828
priyankanyadav <priyankanyadav@gmail.com>
2025-09-05 17:15:34 -0400

    added variables

---
+++ variables.js
@@ -0,0 +1,64 @@
+/* 
+  Developer: Priyanka Yadav
+  Purpose: Practice declaring, updating, and logging variables
+*/
+
+// Complete each of the coding tasks listed below. Place your solution directly under each corresponding task item.
+
+// Task 1. Declare a variable named schoolAddress and initialize it any George Brown College Campus address
+let schoolAddress = "160 Kendal Ave, Toronto, ON M5R 1M3"; // Casa Loma Campus
+
+// Task 2. Display the value of the variable schoolAddress on the browser console
+console.log("Task 2:", schoolAddress);
+
+// Task 3. Update variable schoolAddress and assign another George Brown College Campus address
+schoolAddress = "200 King St E, Toronto, ON M5A 3W8"; // St. James Campus
+
+// Task 4. Display the value of the variable schoolAddress on the browser console
+console.log("Task 4:", schoolAddress);
+
+// Task 5. Declare variable that uses the camel case style and assign any value.
+let studentFirstName = "Priyanka";
+
+// Task 6. Display the value of the variable on the browser console
+console.log("Task 6:", studentFirstName);
+
+// Task 7. Declare variable that uses the snake case style and assign any value.
+let student_last_name = "Yadav";
+
+// Task 8. Display the value of the variable on the browser console
+console.log("Task 8:", student_last_name);
+
+// Task 9. Declare a variable of your choice and assign any string of your choice
+let program = "Computer Engineering";
+
+// Task 10. Display the value of the variable on the browser console
+console.log("Task 10:", program);
+
+// Task 11. Declare a variable of your choice and assign any number of your choice
+let semester = 1;
+
+// Task 12. Display the value of the variable on the browser console
+console.log("Task 12:", semester);
+
+// Task 13. Declare a variable of your choice and assign any boolean of your choice
+let isFullTime = true;
+
+// Task 14. Display the value of the variable on the browser console
+console.log("Task 14:", isFullTime);
+
+// Task 15. Declare a variable of your choice and assign an array of either string or number of your choice
+let courseCodes = ["CST8101", "CST8215", "MAT8001"];
+
+// Task 16. Display the value of the variable on the browser console
+console.log("Task 16:", courseCodes);
+
+// Task 17. Declare a variable named vehicle and assign the following properties and values:
+const vehicle = {
+  make: "Honda",
+  model: "Civic",
+  vin: "1GNEK13Z42R123456",
+};
+
+// Task 18. Display the value of the variable on the browser console
+console.log("Task 18:", vehicle);
